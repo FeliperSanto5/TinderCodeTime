ec49756abc65afa7b933cf19d445da14d37c2c05
Jv12102008 <Jvbonfim13256@gmail.com>
2025-07-24 21:02:57 -0300

    Foi criado o repozitorio com sucesso

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Dashnoard } from "./page/Dashnoard"
+import { Home } from "./page/Home"
+import { Information } from "./page/Information"
+import { Register } from "./page/Register"
+
+function App() {
+
+
+  return (
+<section className="App">
+  <Dashnoard/>
+  <Home/>
+  <Information/>
+  <Register/>
+</section>
+  )
+}
+
+export default App
