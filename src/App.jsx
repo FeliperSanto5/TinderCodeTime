b544e72214699bc1bc556bbeb93cb599f150b783
FeliperSanto5 <felipersantos4@gmail.com>
2025-07-24 21:49:45 -0300

    Adicionadas as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { Dashnoard } from "./page/Dashnoard"
 import { Home } from "./page/Home"
 import { Information } from "./page/Information"
 import { Register } from "./page/Register"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
 
   return (
-<section className="App">
-  <Dashnoard/>
-  <Home/>
-  <Information/>
-  <Register/>
-</section>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" exact element={<Information />} />
+    <Route path="/home" exact element={<Home />}/>
+    <Route path="/register" exact element={<Register />}/>
+    <Route path="/dashboard" exact element={<Dashnoard />}/>
+  </Routes>
+  </BrowserRouter>
   )
 }
 
