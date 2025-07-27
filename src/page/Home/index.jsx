f206b15b771806e20efcc37c5ad9c3bc599e9a78
Jv12102008 <Jvbonfim13256@gmail.com>
2025-07-27 14:29:17 -0300

    Foi criado a parte dos post da tela Home

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -4,6 +4,17 @@ export default function Home(){
     return(
        <section id="Home">
        <Header/>
+       <ul id="posts">
+        <li className="post">
+            <img src="https://i.pinimg.com/736x/90/c0/97/90c097159b9bf2eac99ed4f5a259ca52.jpg" alt="" />
+           <div className="informations">
+            <div className="name">Luiza</div>
+            <div className="person-inf">
+                <p>22. Salvador/Bahia</p>
+            </div>
+           </div>
+        </li>
+       </ul>
        </section>
     )
 }
